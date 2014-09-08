2b3842042f71b643a9cd3581a7c5a8d76e949def
Chakib <c@fasterize.com>
2014-09-08 17:38:10 +0200

    add init counter

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+Commands:
+run : go run app
+vote : /[0..9]
+display result : /status
+init Counter: /initCounterFF00123
--- app.go
+++ app.go
@@ -17,7 +17,9 @@ type All struct {
 	Resultat [10]Stat
 }
 var conf All
-var top_channel <- chan time.Time
+var sync_channel chan bool
+var timer_channel <- chan time.Time
+
 var vote_channel chan int
 func process_vote(){
 
@@ -31,16 +33,24 @@ func process_vote(){
 }
 func sync_to_file(){
     for{
-        <-top_channel
-        str,err := json.Marshal(conf)
+        select {
+            case <-timer_channel:
+                save_to_file()
+            case <-sync_channel:
+                save_to_file()
+            }
+    }
+
+}
+func save_to_file(){
+    fmt.Println("save to file")
+    str,err := json.Marshal(conf)
         if(err== nil){
         fmt.Println("hello",conf,string(str))
         ioutil.WriteFile("compagne.json",str,0644)
         } else {
             fmt.Println("error sync_to_file ",err)
         }
-    }
-
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -54,12 +64,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 }
 func getResult(w http.ResponseWriter, r *http.Request) {
-    /*content, err := ioutil.ReadFile("compagne.json")
-    if err!=nil{
-        fmt.Print("Error:",err)
-
-    }
-    */
     response := "<h1> Vote result</h1>"
     for i:=0;i<10;i++{
         response+= fmt.Sprintf("<span style=\"color:red;margin-right:20px\">Choix %d:</span><span style=\"color:green\">%d hits</span></br>",i,conf.Resultat[i].Votes)
@@ -67,6 +71,15 @@ func getResult(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, response)
 
 
+}
+
+func initCounter(w http.ResponseWriter, r *http.Request) {
+   for i:=0;i<10;i++{
+       conf.Resultat[i].Votes=0
+    }
+    fmt.Println("init counter")
+    sync_channel <- true
+    http.Redirect(w,r,"/status",301)
 }
 
 func main() {
@@ -80,11 +93,13 @@ func main() {
             fmt.Print("Error:",err)
         }
     }
-    top_channel = time.NewTicker(time.Second*30).C
+    timer_channel = time.NewTicker(time.Second*30).C
     vote_channel = make(chan int,1000)
+    sync_channel = make(chan bool)
     go process_vote()
     go sync_to_file()
     http.HandleFunc("/status", getResult)
+    http.HandleFunc("/initCounterFF00123", initCounter)
     http.HandleFunc("/", handler)
     http.ListenAndServe(GetPort(), nil)
 }
